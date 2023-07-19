700d04ff6ad265b08b887883d91c5f0a41b85ba1
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-07-19 21:19:53 +0600

    form group template done

--- components/FormGroup.vue
+++ components/FormGroup.vue
@@ -2,11 +2,24 @@
   <div class="flex flex-col space-y-2">
     <Label>{{ label }}</Label>
     <Input
+      :placeholder="placeholder"
       :type="type"
       :value="value"
       @update:modelValue="$emit('update:modelValue', $event.target.value)"
     />
+    <p v-if="errorMessage" class="text-red-500 text-sm">{{ errorMessage }}</p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  label: String,
+  type: String,
+  placeholder: String,
+  value: String | Number,
+  modelValue: String | Number,
+  errorMessage: String,
+});
+
+defineEmits(["update:modelValue"]);
+</script>
