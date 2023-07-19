7d6a2d7e75647e9fbc80d8a2b937716c9833b14e
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-07-19 21:14:22 +0600

    update form group component

--- components/FormGroup.vue
+++ components/FormGroup.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col space-y-2">
     <Label>{{ label }}</Label>
-    <Input :type="type" :value="value" @update="moduleValue"
+    <Input
+      :type="type"
+      :value="value"
+      @update:modelValue="$emit('update:modelValue', $event.target.value)"
+    />
   </div>
 </template>
 
--- components/Input.vue
+++ components/Input.vue
@@ -4,15 +4,15 @@
     class="w-full border border-gray-700 rounded focus:ring focus:ring-purple-500 outline-none text-gray-600 transition-all duration-150 ease-in-out"
     v-bind="$attrs"
     :value="value"
-    @input="$emit('update:moduleValue', $event.target.value)"
+    @input="$emit('update:modelValue', $event.target.value)"
   />
 </template>
 
 <script setup>
 defineProps({
-  moduleValue: String | Number,
+  modelValue: String | Number,
   value: String | Number,
 });
 
-defineEmits(["update:moduleValue"]);
+defineEmits(["update:modelValue"]);
 </script>
