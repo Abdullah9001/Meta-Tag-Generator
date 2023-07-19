413554d7daf6f4dc529e57112fe6aac423f78987
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-07-20 00:00:54 +0600

    done for today

---
+++ components/MetaTags.vue
@@ -0,0 +1,19 @@
+<template>
+  <Head>
+    <Title>Our App | {{ title }}</Title>
+    <Meta name="description" :content="description" />
+    <Meta property="og:title" :content="title" />
+    <Meta property="og:description" :content="description" />
+    <Meta property="og:url" :content="url || 'http://localhost:3000'" />
+    <Meta property="og:type" :content="website" />
+    <Meta property="og:site_name" :content="MyApp" />
+  </Head>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  description: String,
+  url: String,
+});
+</script>
