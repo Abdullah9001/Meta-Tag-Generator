3307f96798861d7a0228296bc61b484edb327ec1
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-07-19 21:12:02 +0600

    update something

---
+++ components/FormGroup.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col space-y-2">
+    <Label>{{ label }}</Label>
+    <Input :type="type" :value="value" @update="moduleValue"
+  </div>
+</template>
+
+<script setup></script>
